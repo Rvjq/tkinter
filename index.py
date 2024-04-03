416598b9d25806fc6f5707ea60b6ba49a68107be
rvjq <rvjq@cesar.school>
2024-04-03 19:13:04 -0300

    Icon and code cleaning

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ ctk.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, g
 
 app = ctk.CTk()  # create CTk window like you do with the Tk window
 app.geometry(str(sizex)+"x"+str(sizey))
+app.wm_iconbitmap('C:\\Users\\rvjq_cesar\\tkinter\\src\\img\\tic-tac-toe.ico')
 app.title("Tic-Tac-Toe")
 
 def button_callback ():
@@ -25,14 +26,19 @@ buttons = ['A1','A2','A3','B1','B2','B3','C1','C2','C3']
 A1,A2,A3,B1,B2,B3,C1,C2,C3 = [ctk.CTkButton(master=frame_1, command=button_callback, width=100, height=100, text=x, fg_color="transparent") for x in buttons]
 
 #Mass button placement
-A1.place(x=0, y=0)
-A2.place(x=100, y=0)
-A3.place(x=200, y=0)
-B1.place(x=0, y=100)
-B2.place(x=100, y=100)
-B3.place(x=200, y=100)
-C1.place(x=0, y=200)
-C2.place(x=100, y=200)
-C3.place(x=200, y=200)
+
+buttonvar = [A1,A2,A3,B1,B2,B3,C1,C2,C3]
+buttonx, buttony = 0,0
+
+for i in range(len(buttonvar)):
+    if i == 3 or i == 6:
+        buttonx = 0
+        buttony += 100
+        pass
+
+    print(buttonvar[i],buttonx,buttony)
+
+    buttonvar[i].place(x=buttonx, y=buttony)
+    buttonx += 100
 
 app.mainloop()
